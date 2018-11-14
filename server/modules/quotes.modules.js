59e5c3386f41ae5347323e7de3351e9badd1f307
phiathao <phiat11@gmail.com>
2018-11-14 12:39:30 -0600

    quote modules

---
+++ server/modules/quotes.modules.js
@@ -0,0 +1,22 @@
+let quotes = [
+    'Work is the curse of the drinking class.',
+    'The only thing to fear is fear itself',
+    `Fool me once, shame on me, but teach a man to fool me and I'll...`
+];
+
+let index = 0
+
+// return one quote
+// not the same every
+function getQuote(){
+// a loop
+let quoteToReturn = quotes[index]; // save quotes[index] to quoteToReturn before adding
+index +=1;
+// keep out quotes inbounds / reset
+if (index >= quotes.length){
+index = 0;
+}
+return quoteToReturn;
+}
+
+module.exports = getQuote;
--- server/server.js
+++ server/server.js
@@ -1,29 +1,9 @@
 // Bring express from Node
 const express = require('express');
+const getQuote = require('./modules/quotes.modules');
 
 const app = express();
 const PORT = 5000;
-
-let quotes = [
-            'Work is the curse of the drinking class.',
-            'The only thing to fear is fear itself',
-            `Fool me once, shame on me, but teach a man to fool me and I'll...`
-        ];
-
-let index = 0
-
-// return one quote
-// not the same every
-function getQuote(){
-    // a loop
-    let quoteToReturn = quotes[index]; // save quotes[index] to quoteToReturn before adding
-    index +=1;
-    // keep out quotes inbounds / reset
-    if (index >= quotes.length){
-        index = 0;
-    }
-    return quoteToReturn;
-}
 
 app.get('/quote', function(req, res){
     res.send(getQuote());
